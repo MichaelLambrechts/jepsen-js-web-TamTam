0a645c743fc29f531123dd2a08951398a997c4ac
ThibautJanssens <janssensthib@gmail.com>
2019-02-05 10:12:27 +0100

    when click modal disappear + convertor md->html && html->md + minor fix

--- ideaList.md
+++
@@ -1,5 +0,0 @@
-# Here you'll find the ideas to conquer the world like real badies.
-###### (disclaimer: We don't guarantee any success, nor cover for your injuries, time in jail or any other inconveniences)
-
-* Gran Theft Panties
-    - Stealing all the panties so the people have to buy them back to you. Get rich, and rule the world.
--- js/function.js
+++ js/function.js
@@ -1,51 +1,47 @@
 'use strict';
 
-const showdown  = require('showdown');
-const fs = require('fs');
+const showdown = require('showdown');
 const converter = new showdown.Converter();
 
-let modalToLoad = " ";
-
 for (let num = 0; num < localStorage.length; num++) {
-  let idealist = document.createElement("ul");
-  idealist.setAttribute("id", "idea"+num);
-  document.querySelector("#list").appendChild(idealist);
+    let idealist = document.createElement("ul");
+    idealist.setAttribute("id", "idea" + num);
+    document.querySelector("#list").appendChild(idealist);
 
-  let idlist = document.createElement("li");
-  idlist.setAttribute("id", "id"+num);
-  idlist.setAttribute("name", num);
-  idlist.setAttribute("data-toggle", "modal");
-  idlist.setAttribute("data-target", "#exampleModal");
-  idlist.setAttribute("class", "display");
-  document.querySelector("#idea"+num).appendChild(idlist);
-  idlist.innerHTML= JSON.parse(localStorage.getItem(num))["name"]
+    let idlist = document.createElement("li");
+    idlist.setAttribute("id", "id" + num);
+    idlist.setAttribute("name", num);
+    idlist.setAttribute("data-toggle", "modal");
+    idlist.setAttribute("data-target", "#exampleModal");
+    idlist.setAttribute("class", "display");
+    document.querySelector("#idea" + num).appendChild(idlist);
+    idlist.innerHTML = JSON.parse(localStorage.getItem(num))["name"]
 
-  let edlist = document.createElement("li");
-  edlist.setAttribute("id", "edit" + num);
-  edlist.setAttribute("name", num);
-  edlist.setAttribute("data-toggle", "modal");
-  edlist.setAttribute("data-target", "#exampleModal");
-  edlist.setAttribute("class", "edit");
-  document.querySelector("#idea"+num).appendChild(edlist);
+    let edlist = document.createElement("li");
+    edlist.setAttribute("id", "edit" + num);
+    edlist.setAttribute("name", num);
+    edlist.setAttribute("data-toggle", "modal");
+    edlist.setAttribute("data-target", "#exampleModal");
+    edlist.setAttribute("class", "edit");
+    document.querySelector("#idea" + num).appendChild(edlist);
 
-  let edcont = document.createElement("i");
-  edcont.setAttribute("id", "editcont" + num);
-  edcont.setAttribute("name", num);
-  edcont.setAttribute("class", "fas fa-edit");
-  document.querySelector("#edit"+num).appendChild(edcont);
-  
+    let edcont = document.createElement("i");
+    edcont.setAttribute("id", "editcont" + num);
+    edcont.setAttribute("name", num);
+    edcont.setAttribute("class", "fas fa-edit");
+    document.querySelector("#edit" + num).appendChild(edcont);
 
-  let delist = document.createElement("li");
-  delist.setAttribute("id", "del" + num);
-  delist.setAttribute("name", num);
-  delist.setAttribute("class", "delete");
-  document.querySelector("#idea"+num).appendChild(delist);
+    let delist = document.createElement("li");
+    delist.setAttribute("id", "del" + num);
+    delist.setAttribute("name", num);
+    delist.setAttribute("class", "delete");
+    document.querySelector("#idea" + num).appendChild(delist);
 
-  let decont = document.createElement("i");
-  decont.setAttribute("id", "delcont" + num);
-  decont.setAttribute("name", num);
-  decont.setAttribute("class", "fas fa-trash-alt");
-  document.querySelector("#del"+num).appendChild(decont);
+    let decont = document.createElement("i");
+    decont.setAttribute("id", "delcont" + num);
+    decont.setAttribute("name", num);
+    decont.setAttribute("class", "fas fa-trash-alt");
+    document.querySelector("#del" + num).appendChild(decont);
 }
 
 let addlist = document.createElement("button");
@@ -57,218 +53,230 @@ document.querySelector("#add").innerText = "Ajouter un plan";
 
 //adding listener on buttons
 document.querySelector("#add").addEventListener("click", () => {
-  loadModal("add");
+    loadModal("add");
 });
 
 //remove all the elements present in the modal
 function clearModal() {
-  //clear the body of the modal
-  var element = document.querySelector(".modal-body");
-  while (element.firstChild) {
-    element.removeChild(element.firstChild);
-  }
-  //clear the head of the modal
-  var element2 = document.querySelector(".modal-header");
-  while (element2.firstChild) {
-    element2.removeChild(element2.firstChild);
-  }
-  //clear the footer the 
-  var element3 = document.querySelector(".modal-footer");
-  while (element3.firstChild) {
-    element3.removeChild(element3.firstChild);
-  }
+    //clear the body of the modal
+    var element = document.querySelector(".modal-body");
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+    //clear the head of the modal
+    var element2 = document.querySelector(".modal-header");
+    while (element2.firstChild) {
+        element2.removeChild(element2.firstChild);
+    }
+    //clear the footer the 
+    var element3 = document.querySelector(".modal-footer");
+    while (element3.firstChild) {
+        element3.removeChild(element3.firstChild);
+    }
 }
 
 //allow the user to add an idea
 function addIdea() {
-  clearModal();
-  //create a element form in the html
-  let formAdd = document.createElement("form");
-  formAdd.setAttribute("id", "add");
-  document.querySelector(".modal-body").appendChild(formAdd);
+    clearModal();
+    //create a element form in the html
+    let formAdd = document.createElement("form");
+    formAdd.setAttribute("id", "add");
+    document.querySelector(".modal-body").appendChild(formAdd);
 
-  //create a input name in the html
-  let nameInput = document.createElement("input");
-  nameInput.setAttribute("value", "name");
-  nameInput.setAttribute("type", "text");
-  nameInput.setAttribute("id", "name");
-  document.querySelector("#add").appendChild(nameInput);
+    //create a input name in the html
+    let nameInput = document.createElement("input");
+    nameInput.setAttribute("value", "name");
+    nameInput.setAttribute("type", "text");
+    nameInput.setAttribute("id", "name");
+    document.querySelector("#add").appendChild(nameInput);
 
-  //create a input description in the html
-  let descInput = document.createElement("input");
-  descInput.setAttribute("value", "desc");
-  descInput.setAttribute("type", "text");
-  descInput.setAttribute("id", "desc");
-  document.querySelector("#add").appendChild(descInput);
+    //create a input description in the html
+    let descInput = document.createElement("input");
+    descInput.setAttribute("value", "desc");
+    descInput.setAttribute("type", "textarea");
+    descInput.setAttribute("id", "desc");
+    document.querySelector("#add").appendChild(descInput);
 
-  //create a button submit
-  let subInput = document.createElement("button");
-  subInput.setAttribute("value", "submit");
-  subInput.setAttribute("class", "btn btn-primary");
-  subInput.setAttribute("type", "submit");
-  subInput.setAttribute("id", "save");
-  document.querySelector(".modal-footer").appendChild(subInput);
-  document.querySelector("#save").innerText = "Créer le plan";
+    //create a button submit
+    let subInput = document.createElement("button");
+    subInput.setAttribute("value", "submit");
+    subInput.setAttribute("class", "btn btn-primary");
+    subInput.setAttribute("data-dismiss", "modal");
+    subInput.setAttribute("type", "submit");
+    subInput.setAttribute("id", "save");
+    document.querySelector(".modal-footer").appendChild(subInput);
+    document.querySelector("#save").innerText = "Créer le plan";
 
-  //submit form and send it to locale storage
-  document.querySelector("#save").addEventListener("click", () => {
-    let nameIdea = document.getElementById('name').value;
-    let descIdea = document.getElementById('desc').value;
+    //submit form and send it to locale storage
+    document.querySelector("#save").addEventListener("click", () => {
+        let nameIdea = document.getElementById('name').value;
+        let descIdea = convertToHtml(document.getElementById('desc').value);
 
-    let idlength = parseInt(localStorage.length);
-    localStorage.setItem(idlength, JSON.stringify({
-      name: nameIdea,
-      desc: descIdea
-    }));
-  });
+        let idlength = parseInt(localStorage.length);
+        localStorage.setItem(idlength, JSON.stringify({
+            name: nameIdea,
+            desc: descIdea
+        }));
+        //
+    });
 }
 
 // click listener display
-for (let n = 0; n < localStorage.length; n++){
-  document.querySelector("#id"+n).addEventListener("click", () => {
-    let id = document.querySelector("#id"+n).getAttribute('name');
-    console.log(id)
-    loadModal("display", id);
-  })
+for (let n = 0; n < localStorage.length; n++) {
+    document.querySelector("#id" + n).addEventListener("click", () => {
+        let id = document.querySelector("#id" + n).getAttribute('name');
+        console.log(id)
+        loadModal("display", id);
+    })
 }
 
 //allow the user to display the idea
 function displayIdea(id) {
-  clearModal();
+    clearModal();
 
-  let nameInput = document.createElement("p");
-  nameInput.setAttribute("id", "name");
-  document.querySelector(".modal-body").appendChild(nameInput);
-  document.querySelector("#name").innerText = JSON.parse(localStorage.getItem(id))["name"]
+    let nameInput = document.createElement("p");
+    nameInput.setAttribute("id", "name");
+    document.querySelector(".modal-body").appendChild(nameInput);
+    document.querySelector("#name").innerText = JSON.parse(localStorage.getItem(id))["name"]
 
-  let descInput = document.createElement("p");
-  descInput.setAttribute("id", "desc");
-  document.querySelector(".modal-body").appendChild(descInput);
-  document.querySelector("#desc").innerText = JSON.parse(localStorage.getItem(id))["desc"]
+    let descInput = document.createElement("p");
+    descInput.setAttribute("id", "desc");
+    document.querySelector(".modal-body").appendChild(descInput);
+    document.querySelector("#desc").innerHTML = JSON.parse(localStorage.getItem(id))["desc"]
+
+    //create a input comment in the modal
+    let commentInput = document.createElement("input");
+    commentInput.setAttribute("value", "comment");
+    commentInput.setAttribute("type", "textarea");
+    commentInput.setAttribute("id", "comment");
+    document.querySelector("#comment").appendChild(commentInput);
+
+    //create a button submit
+    let subInput = document.createElement("button");
+    subInput.setAttribute("value", "submit");
+    subInput.setAttribute("class", "btn btn-primary");
+    subInput.setAttribute("type", "submit");
+    subInput.setAttribute("id", "save");
+    document.querySelector(".modal-footer").appendChild(subInput);
+    document.querySelector("#save").innerText = "Envoyer le commentaire";
 }
 
 //
 document.querySelector(".display").addEventListener("click", () => {
-  let id = document.querySelector(".display").getAttribute('name');
-  console.log(id)
-  loadModal("display", id);
+    let id = document.querySelector(".display").getAttribute('name');
+    console.log(id)
+    loadModal("display", id);
 })
 
 // click edit listener
-for (let f = 0; f < localStorage.length; f++){
-  document.querySelector("#edit"+f).addEventListener("click", () => {
-    let id = document.querySelector("#edit"+f).getAttribute('name');
-    loadModal("edit", id);
-  })
+for (let f = 0; f < localStorage.length; f++) {
+    document.querySelector("#edit" + f).addEventListener("click", () => {
+        let id = document.querySelector("#edit" + f).getAttribute('name');
+        loadModal("edit", id);
+    })
 }
 
 //allow the user to edit an idea
 function editIdea(id) {
-  clearModal();
-  let name = JSON.parse(localStorage.getItem(id))["name"]
-  let desc = JSON.parse(localStorage.getItem(id))["desc"]
+    clearModal();
+    let name = JSON.parse(localStorage.getItem(id))["name"]
+    let desc = JSON.parse(localStorage.getItem(id))["desc"]
 
-  //create a element form in the html
-  let formEdit = document.createElement("form");
-  formEdit.setAttribute("id", "edit");
-  document.querySelector(".modal-body").appendChild(formEdit);
+    //create a element form in the html
+    let formEdit = document.createElement("form");
+    formEdit.setAttribute("id", "edit");
+    document.querySelector(".modal-body").appendChild(formEdit);
 
-  //create a input name in the html
-  let nameInput = document.createElement("input");
-  nameInput.setAttribute("value", name);
-  nameInput.setAttribute("type", "text");
-  nameInput.setAttribute("id", "name");
-  document.querySelector("#edit").appendChild(nameInput);
+    //create a input name in the html
+    let nameInput = document.createElement("input");
+    nameInput.setAttribute("value", name);
+    nameInput.setAttribute("type", "text");
+    nameInput.setAttribute("id", "name");
+    document.querySelector("#edit").appendChild(nameInput);
 
-  //create a input description in the html
-  let descInput = document.createElement("input");
-  descInput.setAttribute("value", desc);
-  descInput.setAttribute("type", "text");
-  descInput.setAttribute("id", "desc");
-  document.querySelector("#edit").appendChild(descInput);
+    //create a input description in the html
+    let descInput = document.createElement("input");
+    descInput.setAttribute("value", convertToMd(desc));
+    descInput.setAttribute("type", "textarea");
+    descInput.setAttribute("id", "desc");
+    document.querySelector("#edit").appendChild(descInput);
 
-  //create a button submit
-  let subInput = document.createElement("button");
-  subInput.setAttribute("value", "submit");
-  subInput.setAttribute("class", "btn btn-primary");
-  subInput.setAttribute("type", "submit");
-  subInput.setAttribute("id", "save");
-  document.querySelector(".modal-footer").appendChild(subInput);
-  document.querySelector("#save").innerText = "Modifier le plan";
+    //create a button submit
+    let subInput = document.createElement("button");
+    subInput.setAttribute("value", "submit");
+    subInput.setAttribute("class", "btn btn-primary");
+    subInput.setAttribute("data-dismiss", "modal");
+    subInput.setAttribute("type", "submit");
+    subInput.setAttribute("id", "save");
+    document.querySelector(".modal-footer").appendChild(subInput);
+    document.querySelector("#save").innerText = "Modifier le plan";
 
-  //submit form and send it to locale storage
-  document.querySelector("#save").addEventListener("click", () => {
-    let nameIdea = document.getElementById('name').value;
-    let descIdea = document.getElementById('desc').value;
-    localStorage.setItem(id, JSON.stringify({
-      name: nameIdea,
-      desc: descIdea
-    }));
-  });
+    //submit form and send it to locale storage
+    document.querySelector("#save").addEventListener("click", () => {
+        let nameIdea = document.getElementById('name').value;
+        let descIdea = convertToHtml(document.getElementById('desc').value);
+        localStorage.setItem(id, JSON.stringify({
+            name: nameIdea,
+            desc: descIdea
+        }));
+    });
 }
 
 // function click listener remove idea
-for (let r = 0; r < localStorage.length; r++){
-  document.querySelector("#del"+r).addEventListener("click", () => {
-    let id = document.querySelector("#del"+r).getAttribute('name');
-    loadModal("remove", id);
-  })
+for (let r = 0; r < localStorage.length; r++) {
+    document.querySelector("#del" + r).addEventListener("click", () => {
+        let id = document.querySelector("#del" + r).getAttribute('name');
+        loadModal("remove", id);
+    })
 }
 
 //remove idea from localStorage
 function removeIdea(id) {
-  id=parseInt(id);
-  // Déplace les éléments suivants vers le précedent
-  for (let n = id +1 ; n < localStorage.length; n++){
+    id = parseInt(id);
+    // Déplace les éléments suivants vers le précedent
+    for (let n = id + 1; n < localStorage.length; n++) {
 
-    let name = JSON.parse(localStorage.getItem(n))["name"];
-    let desc = JSON.parse(localStorage.getItem(n))["desc"];
-    let idlength = n-1;
-    console.log(idlength)
-    localStorage.setItem(idlength, JSON.stringify({
-      "name": name,
-      "desc": desc
-    }));
-  }
-  localStorage.removeItem(localStorage.length-1);
+        let name = JSON.parse(localStorage.getItem(n))["name"];
+        let desc = JSON.parse(localStorage.getItem(n))["desc"];
+        let idlength = n - 1;
+        console.log(idlength)
+        localStorage.setItem(idlength, JSON.stringify({
+            "name": name,
+            "desc": desc
+        }));
+    }
+    localStorage.removeItem(localStorage.length - 1);
 
 }
 
 //load the modal following the button that the user press
 function loadModal(modalToLoad, id) {
-  switch (modalToLoad) {
-    case "display":
-      displayIdea(id);
-      break;
-    case "add":
-      addIdea();
-      break;
-    case "edit":
-      editIdea(id);
-      break;
-    case "remove":
-      removeIdea(id);
-      break;
-    default:
-      break;
-  }
+    switch (modalToLoad) {
+        case "display":
+            displayIdea(id);
+            break;
+        case "add":
+            addIdea();
+            break;
+        case "edit":
+            editIdea(id);
+            break;
+        case "remove":
+            removeIdea(id);
+            break;
+        default:
+            break;
+    }
 }
 
 //@þaram = string in markdown toConvert  @return = markdown converted in html
-function convertToHtml(toConvert){
-  let toRet = "";
-  toRet += converter.makeHtml(toConvert);
-  return toRet;
+function convertToHtml(toConvert) {
+    let toRet = converter.makeHtml(toConvert);
+    return toRet;
 }
 
-//load file 'ideaList.md' to convert it to html and print it on the page.
-function loadFile() {
-  let obj, toRet = "";
-
-  obj = fs.readFileSync('ideaList.md', 'utf8');
-  toRet += converter.makeHtml(obj);
-  return toRet;
+//@þaram = string in html toConvert  @return = html converted in markdown
+function convertToMd(toConvert) {
+    let toRet = converter.makeMarkdown(toConvert);
+    return toRet;
 }
-
-//document.querySelector("").innerHTML = loadFile();
-
